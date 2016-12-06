534f2f2f7fcd08acbd6c037974b690ff991fc3a5
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-06 10:08:21 +0000

    added input box text

--- public/index.js
+++ public/index.js
@@ -1,20 +1,26 @@
-var clickTotal = 0;
+var clickTotal = 1;
 
 var app = function(){
   // querySelector will return the first element called button
   var button = document.querySelector('button');
   button.onclick = handleButtonClick;
 
+  // querySelector will return text that is entered
+  var inputBox = document.querySelector('input');
+  inputBox.onkeyup = handleKeyPress;
+
+}
+
+var handleKeyPress = function() {
+  var pTag = document.querySelector('#text-result');
+  var input = document.querySelector('input');
+  pTag.innerText = input.value;
 }
 
 var handleButtonClick = function() {
-
   var pTag = document.querySelector('#button-result');
   pTag.innerText = "ive been clicked " + clickTotal + " times";
   clickTotal ++;
-
-
-
 }
 
 
